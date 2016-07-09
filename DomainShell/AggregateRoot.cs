6d8791a0ce1badb114685ff50d754b083a3966fa
kawa <kyoutetu@gmail.com>
2016-07-09 23:22:56 +0900

    update

---
+++ DomainShell/AggregateRoot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using DomainShell.Base;
+using DomainShell.Infrastructure;
+
+namespace DomainShell
+{
+    public interface IAggregateRoot
+    {
+    }
+}
