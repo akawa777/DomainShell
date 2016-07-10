86d51531d4178df2ab42465b8c5731d30381adc4
kawa <kyoutetu@gmail.com>
2016-07-10 21:57:42 +0900

    update

---
+++ DomainShell.Tests.Web/Scripts/view.js
@@ -0,0 +1,27 @@
+﻿(function (definition) {
+    // CommonJS
+    if (typeof exports === "object") {
+        module.exports = definition();
+
+        // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+        define(definition);
+
+        // <script>
+    } else {
+        view = definition();
+    }
+
+})(function () {// 実際の定義を行う関数
+    'use strict';
+
+    var view = function (options) {
+        var context = function (selector) {
+            return $(options.el).find(selector);
+        }
+
+        new options.app(context, options.parameters);
+    }
+
+    return view;
+});
