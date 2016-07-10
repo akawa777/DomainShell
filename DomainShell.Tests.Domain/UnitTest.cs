eb78dea04f66ccc7a98619992957a1be745a1ea8
kawa <kyoutetu@gmail.com>
2016-07-11 01:13:13 +0900

    update

---
+++ DomainShell.Tests.Domain/UnitTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainShell.Tests.Domain.Models;
+using DomainShell.Tests.Domain.Service;
+
+namespace DomainShell.Tests
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Main()
+        {
+            Person person = new Person();
+
+            person.Name = "add";
+
+            person.Add();
+
+            PersonReader reader = new PersonReader();
+
+            person = reader.Get(person.Id);
+
+            Assert.AreEqual("add", person.Name);
+
+            person.Name = "update";
+
+            person.Update();
+
+            person = reader.Get(person.Id);
+
+            Assert.AreEqual("update", person.Name);
+
+            person.Remove();
+
+            person = reader.Get(person.Id);
+
+            Assert.AreEqual(null, person);
+
+            Person[] persons = reader.GetAll();
+
+            string[] ids = persons.Select(x => x.Id).ToArray();
+
+            PersonBulkUpdate bulk = new PersonBulkUpdate();            
+
+            bulk.BulkUpdate(ids, "bulk");
+
+            persons = reader.GetAll();
+
+            foreach (Person item in persons)
+            {
+                Assert.AreEqual("bulk", item.Name);
+            }
+        }
+    }
+}
