edb7180a1414fc9233bbbf5c93835d25a302f009
kawa <kyoutetu@gmail.com>
2016-07-28 01:21:47 +0900

    update

---
+++ DomainShell.Tests.Web/Scripts/Person/modal.js
@@ -0,0 +1,23 @@
+﻿define(["coco", "text!/views/person/modal"], function (coco, template) {
+    var modal = {
+        node: template,
+        ready: function () {
+            var self = this;
+            
+            self.$context(".modal-body").text(self.$params.body);
+
+            self.$context(".btn-primary").on("click", function () {
+                var name = self.$context("[name=name]").val();
+                $(self.$params.nameTextEl).val(name);
+                self.$context(".modal").modal('hide');
+            });
+        },
+        show: function () {
+            var self = this;
+
+            self.$context(".modal").modal('show');
+        }
+    }
+
+    return modal;
+});
