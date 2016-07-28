fec3e305d15f273f7d500008d5b8b714d585fde1
kawa <kyoutetu@gmail.com>
2016-07-29 01:14:38 +0900

    update

---
+++ DomainShell.Tests.Web/Scripts/Person/list2.js
@@ -0,0 +1,31 @@
+﻿require(["el", "coco", "text!/views/person/list2"], function (el, coco, template) {
+    var list = {
+        node: template,
+        init: function() {
+            var self = this;
+
+            self.$components = {
+                tr: {
+                    model: {
+                        ready: function () {
+                            var self = this;
+                            self.$context(".id").text(self.$params.Id);
+                            self.$context(".name").text(self.$params.Name);
+                        }
+                    },
+                    eachParams: [
+                        { Id: 1, Name: "1_name" },
+                        { Id: 2, Name: "2_name" },
+                        { Id: 3, Name: "3_name" },
+                    ]
+                }
+            }
+        }
+    }
+
+    var view = coco({
+        model: list
+    });
+
+    $(el).replaceWith(view.el);
+});   
