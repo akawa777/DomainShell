364e7c2d4be98f1e9f9abdf9a36a944f66944b55
kawa <kyoutetu@gmail.com>
2016-07-31 23:29:50 +0900

    update

--- DomainShell.Tests.Web/Scripts/Person/list2.js
+++ DomainShell.Tests.Web/Scripts/Person/list2.js
@@ -1,4 +1,13 @@
 ﻿require(["el", "coco", "text!/views/person/list2"], function (el, coco, template) {
+    var persons = [
+                { Id: 1, Name: "1_name" },
+                { Id: 2, Name: "2_name" },
+                { Id: 3, Name: "3_name" },
+                { Id: 4, Name: "4_name" },
+                { Id: 5, Name: "5_name" },
+                { Id: 6, Name: "6_name" }
+    ];
+
     var list = {
         node: template,        
         ready: function () {
@@ -35,21 +44,19 @@
                 }
             }
 
-            var eachParams = [
-                { Id: 1, Name: "1_name" },
-                { Id: 2, Name: "2_name" },
-                { Id: 3, Name: "3_name" }
-            ];
+            var showList = function (persons) {
+                self.$context("table tbody").empty();
+                persons.forEach(function (person) {
+                    var view = self.$coco({
+                        model: tr,
+                        params: person
+                    });
 
-            self.$context("table tbody").empty();
-            eachParams.forEach(function (params) {
-                var view = self.$coco({
-                    model: tr,
-                    params: params
+                    self.$context("table tbody").append(view.el);
                 });
+            }
 
-                self.$context("table tbody").append(view.el);
-            });
+            showList(persons.slice(0, 3));            
 
             self.$context("[name=new]").on("click", function () {
                 var view = self.$coco({
@@ -59,8 +66,15 @@
 
                 self.$context("table tbody").append(view.el);
             });
-        }
 
+            self.$context("[name=prev]").on("click", function () {
+                showList(persons.slice(0, 3));
+            });
+
+            self.$context("[name=next]").on("click", function () {
+                showList(persons.slice(3, persons.length));
+            });
+        }
     }
 
     var view = coco({
