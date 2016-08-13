a2fe435de737fb4c3c980ef3530f2f0b5ae5455a
kawa <kyoutetu@gmail.com>
2016-08-13 12:09:09 +0900

    update

---
+++ DomainShell.Tests.Domain/Payment/ICreditCardService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainShell.Domain;
+
+namespace DomainShell.Tests.Domain.Payment
+{
+    public interface ICreditCardService<TAggregateRoot> 
+        : IService<TAggregateRoot> where TAggregateRoot : IAggregateRoot
+    {
+        void Pay(TAggregateRoot aggregateRoot);
+    }
+}
